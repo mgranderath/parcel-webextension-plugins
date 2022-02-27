b906d6175cef1ebcb3d43e2fe5ea6a86599550bd
mgranderath <malte.granderath+github@gmail.com>
2022-02-27 15:48:10 +0100

    Changing to support new API

--- packages/parcel-resolver-manifest-merge/src/WebExtensionResolver.js
+++ packages/parcel-resolver-manifest-merge/src/WebExtensionResolver.js
@@ -5,32 +5,32 @@ import merge from "deepmerge";
 const NODE_ENV = process.env.NODE_ENV ?? "development";
 
 export default new Resolver({
-  async resolve({ options, filePath }) {
-    if (path.basename(filePath) !== "manifest.json") {
+  async resolve({ options, specifier }) {
+    if (path.basename(specifier) !== "manifest.json") {
       return null;
     }
 
     const fs = options.inputFS;
 
-    const directoryOfManifest = path.dirname(filePath);
-    const mainManifestCode = JSON.parse(await fs.readFile(filePath, "utf-8"));
+    const directoryOfManifest = path.dirname(specifier);
+    const mainManifestCode = JSON.parse(await fs.readFile(specifier, "utf-8"));
 
     const additionalManifestPath = path.join(
       directoryOfManifest,
       `manifest.${NODE_ENV}.json`
     );
     let additionalManifestCode = {};
-    if (fs.exists(additionalManifestPath)) {
+    if (await fs.exists(additionalManifestPath)) {
       additionalManifestCode = JSON.parse(
         await fs.readFile(additionalManifestPath, "utf-8")
       );
     }
 
     return {
-      filePath: await fs.realpath(filePath),
+      filePath: await fs.realpath(specifier),
       code: JSON.stringify(merge(mainManifestCode, additionalManifestCode)),
       invalidateOnFileCreate: [],
-      invalidateOnFileChange: [],
+      invalidateOnFileChange: [additionalManifestPath],
     };
   },
 });
